7fe4f38d4d8f0599e7f081d0ceccde517aceb170
Benjamin Derksen <45900418+BennyDeeDev@users.noreply.github.com>
2024-07-27 14:30:25 +0200

    score text

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <stdio.h>
 
 int main(void)
 {
@@ -10,27 +11,48 @@ int main(void)
 
     const int screenWidthCenter = 1280 / 2;
     const int screenHeightCenter = 720 / 2;
+
     const int rectangleWidth = 14;
     const int rectangleHeight = 64;
+
     const int paddingX = 48;
+    const int paddingY = 48;
     const int moveY = rectangleHeight / 2;
+
     const int ballRadius = 8;
     const float ballSpeed = 80;
 
     const Color rectangleColor = LIME;
     const Color ballColor = PURPLE;
+    const Color textColor = RAYWHITE;
 
     Vector2 rectangleLeftPos = {paddingX, screenHeightCenter - (rectangleHeight / 2)};
-    Vector2 ballPos = {screenWidthCenter, screenHeightCenter};
     Vector2 rectangleRightPos = {screenWidth - paddingX - rectangleWidth, screenHeightCenter - (rectangleHeight / 2)};
+
+    Vector2 ballPos = {screenWidthCenter, screenHeightCenter};
     Vector2 ballVelocity = {4, 4};
+
     bool isGameStarted = false;
+    int leftPlayerScore = 0;
+    int rightPlayerScore = 0;
+    char leftPlayerScoreText[10];
+    char rightPlayerScoreText[10];
+
+    Vector2 leftPlayerScorePos = {paddingX, paddingY};
+    Vector2 rightPlayerScorePos = {screenWidth - paddingX, paddingY};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
         ClearBackground(BLACK);
+
+        if (leftPlayerScore >= 11 || rightPlayerScore >= 11)
+        {
+            isGameStarted = false;
+            leftPlayerScore = 0;
+            rightPlayerScore = 0;
+        }
 
         if (IsKeyPressed(KEY_W))
         {
@@ -59,19 +81,19 @@ int main(void)
 
         if (ballPos.x <= 0)
         {
-            // Ball has passed the left boundary (possibly a score event)
+            rightPlayerScore = rightPlayerScore + 1;
             isGameStarted = false;
         }
-        else if (ballPos.x >= 1280)
+        else if (ballPos.x >= screenWidth)
         {
-            // Ball has passed the right boundary (possibly a score event)
+            leftPlayerScore = leftPlayerScore + 1;
             isGameStarted = false;
         }
         else if (ballPos.y <= 0)
         {
             ballVelocity.y = -ballVelocity.y;
         }
-        else if (ballPos.y >= 720)
+        else if (ballPos.y >= screenHeight)
         {
             ballVelocity.y = -ballVelocity.y;
         }
@@ -81,6 +103,15 @@ int main(void)
         DrawCircle(ballPos.x, ballPos.y, ballRadius, ballColor);
 
         DrawRectangle(rectangleRightPos.x, rectangleRightPos.y, rectangleWidth, rectangleHeight, rectangleColor);
+
+        sprintf(leftPlayerScoreText, "Score: %d", leftPlayerScore);
+        DrawText(leftPlayerScoreText, leftPlayerScorePos.x, leftPlayerScorePos.y, 22, textColor);
+
+        sprintf(rightPlayerScoreText, "Score: %d", rightPlayerScore);
+
+        int rightPlayerScoreTextSize;
+        rightPlayerScoreTextSize = MeasureText(rightPlayerScoreText, 22);
+        DrawText(rightPlayerScoreText, rightPlayerScorePos.x - rightPlayerScoreTextSize, rightPlayerScorePos.y, 22, textColor);
 
         EndDrawing();
     }
