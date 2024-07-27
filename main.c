746856e5bc549859ea1cca3fb718b1942e5d1e0a
Benjamin Derksen <45900418+BennyDeeDev@users.noreply.github.com>
2024-07-27 19:28:24 +0200

    winnerText, playerBounds, spacebarText

--- main.c
+++ main.c
@@ -19,18 +19,15 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Pong Raylib C");
     SetTargetFPS(60);
 
-    const int screenWidthCenter = 1280 / 2;
-    const int screenHeightCenter = 720 / 2;
+    const int screenWidthCenter = screenWidth / 2;
+    const int screenHeightCenter = screenHeight / 2;
 
     const int rectangleWidth = 14;
     const int rectangleHeight = 64;
 
     const int paddingX = 48;
     const int paddingY = 48;
-    const int moveY = rectangleHeight / 2;
-
-    const int ballRadius = 8;
-    const float ballSpeed = 80;
+    const int moveY = 4;
 
     const Color rectangleColor = LIME;
     const Color ballColor = PURPLE;
@@ -39,17 +36,37 @@ int main(void)
     Vector2 rectangleLeftPos = {paddingX, screenHeightCenter - (rectangleHeight / 2)};
     Vector2 rectangleRightPos = {screenWidth - paddingX - rectangleWidth, screenHeightCenter - (rectangleHeight / 2)};
 
+    const int ballRadius = 8;
+    const float ballSpeed = 4;
     Vector2 ballPos = {screenWidthCenter, screenHeightCenter};
-    Vector2 ballVelocity = {4, 4};
+    Vector2 ballVelocity = {ballSpeed, ballSpeed};
 
-    bool isGameStarted = false;
+    bool didGameStart = false;
+
+    // 0 => No Winner
+    // 1 => Player 1 won!
+    // 2 => Player 2 won!
+    int winner = 0;
+
+    char winnerText[50];
+    int fontSizeWinner = 48;
+    int paddingYWinner = 128;
+    const Color winnerColor = GOLD;
+
     int leftPlayerScore = 0;
     int rightPlayerScore = 0;
     char leftPlayerScoreText[10];
     char rightPlayerScoreText[10];
 
-    Vector2 leftPlayerScorePos = {paddingX, paddingY};
-    Vector2 rightPlayerScorePos = {screenWidth - paddingX, paddingY};
+    char *spacebarText = "Press Spacebar to play!";
+    int fontSizeSpacebar = 36;
+    int paddingYSpacebar = 256;
+
+    int fontSizeScore = 36;
+    int paddingXScore = 64;
+    Vector2 leftPlayerScorePos = {paddingX + paddingXScore, paddingY};
+    Vector2 rightPlayerScorePos = {screenWidth - paddingX - paddingXScore, paddingY};
+    int scoreMax = 11;
 
     while (!WindowShouldClose())
     {
@@ -57,34 +74,38 @@ int main(void)
 
         ClearBackground(BLACK);
 
-        if (leftPlayerScore >= 11 || rightPlayerScore >= 11)
+        if (leftPlayerScore >= scoreMax || rightPlayerScore >= scoreMax)
         {
-            isGameStarted = false;
+            didGameStart = false;
+            // TODO: Don't set it to 0 so you can see the Score when someone wins
             leftPlayerScore = 0;
             rightPlayerScore = 0;
+
+            winner = leftPlayerScore >= scoreMax ? 1 : 2;
         }
 
-        if (IsKeyPressed(KEY_W))
+        if (IsKeyDown(KEY_W) && rectangleLeftPos.y >= 0)
         {
             rectangleLeftPos.y = rectangleLeftPos.y - moveY;
         }
-        else if (IsKeyPressed(KEY_S))
+        else if (IsKeyDown(KEY_S) && rectangleLeftPos.y <= screenHeight - rectangleHeight)
         {
             rectangleLeftPos.y = rectangleLeftPos.y + moveY;
         }
 
-        if (IsKeyPressed(KEY_UP))
+        if (IsKeyDown(KEY_UP) && rectangleRightPos.y >= 0)
         {
             rectangleRightPos.y = rectangleRightPos.y - moveY;
         }
-        else if (IsKeyPressed(KEY_DOWN))
+        else if (IsKeyDown(KEY_DOWN) && rectangleRightPos.y <= screenHeight - rectangleHeight)
         {
             rectangleRightPos.y = rectangleRightPos.y + moveY;
         }
 
-        if (IsKeyPressed(KEY_SPACE) && !isGameStarted)
+        if (IsKeyPressed(KEY_SPACE) && !didGameStart)
         {
-            isGameStarted = true;
+            didGameStart = true;
+            winner = 0;
 
             srand(time(NULL));
 
@@ -94,7 +115,7 @@ int main(void)
             }
         }
 
-        if (isGameStarted)
+        if (didGameStart)
         {
             ballPos.x = ballPos.x + ballVelocity.x;
             ballPos.y = ballPos.y + ballVelocity.y;
@@ -108,12 +129,12 @@ int main(void)
         if (ballPos.x <= 0)
         {
             rightPlayerScore = rightPlayerScore + 1;
-            isGameStarted = false;
+            didGameStart = false;
         }
         else if (ballPos.x >= screenWidth)
         {
             leftPlayerScore = leftPlayerScore + 1;
-            isGameStarted = false;
+            didGameStart = false;
         }
         else if (ballPos.y <= 0)
         {
@@ -136,13 +157,24 @@ int main(void)
         DrawRectangle(rectangleRightPos.x, rectangleRightPos.y, rectangleWidth, rectangleHeight, rectangleColor);
 
         sprintf(leftPlayerScoreText, "Score: %d", leftPlayerScore);
-        DrawText(leftPlayerScoreText, leftPlayerScorePos.x, leftPlayerScorePos.y, 22, textColor);
+        DrawText(leftPlayerScoreText, leftPlayerScorePos.x, leftPlayerScorePos.y, fontSizeScore, textColor);
 
         sprintf(rightPlayerScoreText, "Score: %d", rightPlayerScore);
 
-        int rightPlayerScoreTextSize;
-        rightPlayerScoreTextSize = MeasureText(rightPlayerScoreText, 22);
-        DrawText(rightPlayerScoreText, rightPlayerScorePos.x - rightPlayerScoreTextSize, rightPlayerScorePos.y, 22, textColor);
+        DrawText(rightPlayerScoreText, rightPlayerScorePos.x - MeasureText(rightPlayerScoreText, fontSizeScore), rightPlayerScorePos.y, fontSizeScore, textColor);
+
+        if (winner)
+        {
+            sprintf(winnerText, "Player %d won!", winner);
+            int winnerTextSize;
+
+            DrawText(winnerText, screenWidthCenter - (MeasureText(winnerText, fontSizeWinner) / 2), paddingYWinner, fontSizeWinner, winnerColor);
+        }
+
+        if (!didGameStart)
+        {
+            DrawText(spacebarText, screenWidthCenter - (MeasureText(spacebarText, fontSizeSpacebar) / 2), paddingYSpacebar, fontSizeSpacebar, textColor);
+        }
 
         EndDrawing();
     }
